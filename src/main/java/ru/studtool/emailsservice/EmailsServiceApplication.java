2ec4eda7dcb13a7dc6960b698ee3c39a4fbb6b49
denis96z <zinoviev.denis@yandex.ru>
2019-05-09 17:19:12 +0300

    initial commit

---
+++ src/main/java/ru/studtool/emailsservice/EmailsServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.studtool.emailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailsServiceApplication.class, args);
+    }
+}
